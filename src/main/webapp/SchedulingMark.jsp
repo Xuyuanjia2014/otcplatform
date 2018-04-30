410f02dc0e08e117dbefa2354cc4bedd419b5109
Xuyuanjia2014 <xuyuanjia2017@otcaix.iscas.ac.cn>
2018-05-01 02:31:25 +0800

    Initial commit

---
+++ src/main/webapp/SchedulingMark.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Xuyuanjia
+  Date: 2018/5/1
+  Time: 1:45
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Title</title>
+
+    <script src="libs/echarts.min.js"></script>
+
+</head>
+<body>
+<div id="main" style="width: 600px;height:400px;"></div>
+</body>
+
+<script src="scripts/SchedulingMark.js" charset="UTF-8"></script>
+</html>
---
+++ src/main/webapp/scripts/SchedulingMark.js
@@ -0,0 +1,26 @@
+option = {
+    tooltip : {
+        formatter: "{a} <br/>{b} : {c}%"
+    },
+    toolbox: {
+        feature: {
+            restore: {},
+            saveAsImage: {}
+        }
+    },
+    series: [
+        {
+            name: '业务指标',
+            type: 'gauge',
+            detail: {formatter:'{value}%'},
+            data: [{value: 50, name: '完成率'}]
+        }
+    ]
+};
+
+var myChart = echarts.init(document.getElementById('main'));
+
+setInterval(function () {
+    option.series[0].data[0].value = (Math.random() * 100).toFixed(2) - 0;
+    myChart.setOption(option, true);
+},2000);
