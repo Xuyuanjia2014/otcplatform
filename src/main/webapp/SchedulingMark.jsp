58b506cbe3d69459b4d459795da92278344f5fc8
Xuyuanjia2014 <xuyuanjia2017@otcaix.iscas.ac.cn>
2018-05-01 14:40:27 +0800

    拓扑图。

--- src/main/webapp/SchedulingMark.jsp
+++ src/main/webapp/SchedulingMark.jsp
@@ -10,12 +10,16 @@
 <head>
     <meta charset="utf-8">
     <title>Title</title>
-
+    <link rel="stylesheet" href="styles/SchedulingMark.css" type="text/css" />
     <script src="libs/echarts.min.js"></script>
 
 </head>
 <body>
-<div id="main" style="width: 600px;height:400px;"></div>
+<div id="content">
+    <div id="main" style="width: 600px;height:400px; float: left;"></div>
+    <div id="graph" style="width: 700px;height:600px; float: left;"></div>
+    <button onclick="reset2Graph()" style="float:right; margin-right: 170px;background-color: #46b3f8; color: #FFFFFF; font-size: 16px; padding: 0px;">优化实例调度</button>
+</div>
 </body>
 
 <script src="scripts/SchedulingMark.js" charset="UTF-8"></script>
--- src/main/webapp/scripts/SchedulingMark.js
+++ src/main/webapp/scripts/SchedulingMark.js
@@ -1,4 +1,4 @@
-option = {
+var option = {
     tooltip : {
         formatter: "{a} <br/>{b} : {c}%"
     },
@@ -13,14 +13,194 @@ option = {
             name: '调度评分',
             type: 'gauge',
             detail: {formatter:'{value}'},
-            data: [{value: 50, name: '调度效果'}]
+            data: [{value: 5, name: '调度效果评分'}]
         }
     ]
 };
 
-var myChart = echarts.init(document.getElementById('main'));
+var initValue = 60;
 
-setInterval(function () {
-    option.series[0].data[0].value = (Math.random() * 100).toFixed(2) - 0;
+var myChart = echarts.init(document.getElementById('main'));
+var startInterval = setInterval(function () {
+    if(option.series[0].data[0].value < initValue)
+    option.series[0].data[0].value += Math.floor(Math.random()*10);
     myChart.setOption(option, true);
-},2000);
+},300);
+//window.clearInterval(startInterval);
+
+
+var option2 = {
+    title: {
+        text: '调度拓扑图'
+    },
+    tooltip: {},
+    animationDurationUpdate: 5500,
+    animationEasingUpdate: 'quinticInOut',
+    series : [
+        {
+            type: 'graph',
+            layout: 'none',
+            symbolSize: 80,
+            roam: true,
+            label: {
+                normal: {
+                    show: true
+                }
+            },
+            edgeSymbol: ['circle', 'arrow'],
+            edgeSymbolSize: [4, 10],
+            edgeLabel: {
+                normal: {
+                    textStyle: {
+                        fontSize: 20
+                    }
+                }
+            },
+            data: [{
+                name: 'Pod1',
+                x: 100,
+                y: 100
+            }, {
+                name: 'Pod2',
+                x: 100,
+                y: 300
+            }, {
+                name: 'Pod3',
+                x: 100,
+                y: 550
+            }, {
+                name: 'Kube-master',
+                x: 550,
+                y: 100
+            },{
+                name: 'Kube-node1',
+                x: 550,
+                y: 300
+            },{
+                name: 'Kube-node2',
+                x: 550,
+                y: 550
+            }],
+            // links: [],
+            links: [ {
+                source: 'Pod1',
+                target: 'Kube-node1'
+            }, {
+                source: 'Pod2',
+                target: 'Kube-node1'
+            }, {
+                source: 'Pod3',
+                target: 'Kube-node1'
+            }, {
+                source: 'Pod3',
+                target: 'Kube-node2'
+            }],
+            lineStyle: {
+                normal: {
+                    opacity: 0.9,
+                    width: 5,
+                    curveness: 0.1
+                }
+            }
+        }
+    ]
+};
+
+var option3 = {
+    title: {
+        text: '调度拓扑图'
+    },
+    tooltip: {},
+    animationDurationUpdate: 5500,
+    animationEasingUpdate: 'quinticInOut',
+    series : [
+        {
+            type: 'graph',
+            layout: 'none',
+            symbolSize: 80,
+            roam: true,
+            label: {
+                normal: {
+                    show: true
+                }
+            },
+            edgeSymbol: ['circle', 'arrow'],
+            edgeSymbolSize: [4, 10],
+            edgeLabel: {
+                normal: {
+                    textStyle: {
+                        fontSize: 20
+                    }
+                }
+            },
+            data: [{
+                name: 'Pod1',
+                x: 100,
+                y: 100
+            }, {
+                name: 'Pod2',
+                x: 100,
+                y: 300
+            }, {
+                name: 'Pod3',
+                x: 100,
+                y: 550
+            }, {
+                name: 'Kube-master',
+                x: 550,
+                y: 100
+            },{
+                name: 'Kube-node1',
+                x: 550,
+                y: 300
+            },{
+                name: 'Kube-node2',
+                x: 550,
+                y: 550
+            }],
+            // links: [],
+            links: [ {
+                source: 'Pod1',
+                target: 'Kube-node1'
+            },{
+                source: 'Pod1',
+                target: 'Kube-master'
+            }, {
+                source: 'Pod2',
+                target: 'Kube-master'
+            },  {
+                source: 'Pod2',
+                target: 'Kube-node2'
+            },{
+                source: 'Pod3',
+                target: 'Kube-node1'
+            }, {
+                source: 'Pod3',
+                target: 'Kube-node2'
+            }],
+            lineStyle: {
+                normal: {
+                    opacity: 0.9,
+                    width: 5,
+                    curveness: 0.1
+                }
+            }
+        }
+    ]
+};
+
+var myChart2 = echarts.init(document.getElementById('graph'));
+myChart2.setOption(option2, true);
+
+var reset2Graph = function(){
+    window.clearInterval(startInterval);
+    initValue = 80;
+    option.series[0].data[0].value = 0;
+    myChart.setOption(option, true);
+    setInterval(function () {
+        if(option.series[0].data[0].value < initValue)
+            option.series[0].data[0].value += Math.floor(Math.random()*10);
+        myChart.setOption(option, true);
+    },300);
+    myChart2.setOption(option3, true);
+}
